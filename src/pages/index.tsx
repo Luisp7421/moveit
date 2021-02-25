fdce1d51f2b8ed6b3e604bf4e2bce14b13c70259
Luis Paulo Ferreira Martins <a42578@alunos.ipb.pt>
2021-02-25 21:48:33 +0000

    feat: add ExperienceBar component

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { ExperienceBar } from "../componentes/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
