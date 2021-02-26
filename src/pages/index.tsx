0914d82353ab85983c95c8687eadc267618a780a
Luis Paulo Ferreira Martins <a42578@alunos.ipb.pt>
2021-02-26 22:31:46 +0000

    feat: add component countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import { ExperienceBar } from "../componentes/ExperienceBar";
 import { Profile } from "../componentes/Profile";
-import { CompletedChanllenger } from "../componentes/CompletedChallenger";
+import { CompletedChallenges } from "../componentes/CompletedChallenges";
+import { Countdown } from "../componentes/Countdown";
 
+import Head from "next/head";
 import styles from "../styles/pages/Home.module.css";
+import React from "react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
-          <CompletedChanllenger />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
